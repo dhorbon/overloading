45e225ce2d09f4207be6246a20cc1ead1d87e0da
dhorbon <szymon080206@gmail.com>
2024-03-04 11:06:14 +0100

    Add project files.

---
+++ overloading/Classes.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace overloading
+{
+    public class Produkt
+    {
+        public double Cena;
+        public string Nazwa;
+        public int Ilosc;
+
+        public Produkt(int cena, string nazwa, int ilosc)
+        {
+            Cena = cena;
+            Nazwa = nazwa;
+            Ilosc = ilosc;
+        }
+
+        public string Info()
+        {
+            return $"{Nazwa}, {Cena}zl\n{Ilosc} sztuk";
+        }
+
+        public static bool operator ==(Produkt a, Produkt b)
+        {
+            if (a.Cena == b.Cena) return true; return false;
+        }
+
+        public static bool operator !=(Produkt a, Produkt b)
+        {
+            return !(a == b);
+        }
+
+        public static bool operator >(Produkt a, Produkt b)
+        {
+            if (a.Cena > b.Cena) return true; return false;
+        }
+
+        public static bool operator <(Produkt a, Produkt b)
+        {
+            if (a.Cena < b.Cena) return true; return false;
+        }
+
+        public static bool operator <=(Produkt a, Produkt b)
+        {
+            if (a.Cena <= b.Cena) return true; return false; 
+        }
+
+        public static bool operator >=(Produkt a, Produkt b)
+        {
+            if (a.Cena >= b.Cena) return true; return false;
+        }
+
+        public static Produkt operator ++(Produkt a)
+        {
+            a.Cena++; return a;
+        }
+
+        public static Produkt operator --(Produkt a)
+        {
+            a.Cena--; return a;
+        }
+    }
+
+    public class Magazyn
+    {
+        public string Nazwa;
+        public List<Produkt> stan = new();
+
+        public Magazyn(string nazwa)
+        {
+            this.Nazwa = nazwa;
+        }
+
+        public void Produkty()
+        {
+            for(int i = 0; i < stan.Count; i++)
+            {
+                Console.WriteLine(stan[i].Info());
+            }
+            Console.WriteLine();
+        }
+
+        public void Sort()
+        {
+            for(int i = 0; i < stan.Count; i++)
+            {
+                for(int j = 1; j < stan.Count; j++)
+                {
+                    if (stan[j] < stan[j - 1])
+                    {
+                        Produkt temp = stan[j];
+                        stan[j] = stan[j - 1];
+                        stan[j - 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+
+    public class Macierz
+    {
+        public List<List<int>> matrix;
+
+        public Macierz(Macierz m)
+        {
+            this.matrix = m.matrix;
+        }
+        public Macierz(List<List<int>> matrix)
+        {
+            this.matrix = matrix;
+        }
+
+        public void Show()
+        {
+            for(int i = 0; i < matrix.Count; i++)
+            {
+                for(int j = 0; j< matrix[i].Count; j++)
+                {
+                    Console.Write(matrix[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        public static List<List<int>> operator +(Macierz a, Macierz b)
+        {
+            for(int i = 0;i < a.matrix.Count; i++)
+            {
+                for(int j = 0; j < a.matrix[i].Count; j++)
+                {
+                    a.matrix[i][j] += b.matrix[i][j];
+                }
+            }
+            return a.matrix;
+        }
+
+        public static List<List<int>> operator -(Macierz a, Macierz b)
+        {
+            for (int i = 0; i < a.matrix.Count; i++)
+            {
+                for (int j = 0; j < a.matrix[i].Count; j++)
+                {
+                    a.matrix[i][j] -= b.matrix[i][j];
+                }
+            }
+            return a.matrix;
+        }
+
+        public static Macierz operator ++(Macierz a)
+        {
+            for (int i = 0; i < a.matrix.Count; i++)
+            {
+                for (int j = 0; j < a.matrix[i].Count; j++)
+                {
+                    a.matrix[i][j] += 2;
+                }
+            }
+            return a;
+        }
+
+        public static Macierz operator --(Macierz a)
+        {
+            for (int i = 0; i < a.matrix.Count; i++)
+            {
+                for (int j = 0; j < a.matrix[i].Count; j++)
+                {
+                    a.matrix[i][j] -= 2;
+                }
+            }
+            return a;
+        }
+
+        public static List<List<int>> operator *(Macierz a, Macierz b)
+        {
+            Macierz c = new(a.matrix);
+            for (int i = 0; i < a.matrix.Count; i++)
+            {
+                for (int j = 0; j < a.matrix[i].Count; j++)
+                {
+                    int sum = 0;
+                    for (int k = 0; k < a.matrix.Count; k++)
+                    {
+                        sum += a.matrix[i][k] * b.matrix[k][j];
+                    }
+                    c.matrix[i][j] = sum;
+                }
+            }
+            return c.matrix;
+        }
+    }
+}
---
+++ overloading/Program.cs
@@ -0,0 +1,56 @@
+﻿using overloading;
+
+Magazyn m = new("magazyn");
+m.stan.Add(new Produkt(5, "1", 1));
+m.stan.Add(new Produkt(15, "2", 2));
+m.stan.Add(new Produkt(10, "3", 3));
+
+m.Produkty();
+m.Sort();
+m.Produkty();
+
+
+List<List<int>> l1 = new(); 
+for(int i = 0; i < 3; i++)
+{
+    l1.Add(new());
+    for(int j = 0; j < 3; j++)
+    {
+        l1[i].Add(i+j);
+    }
+}
+
+Macierz matrix = new(l1);
+
+for (int i = 0; i < l1.Count; i++)
+{
+    for (int j = 0; j < l1[i].Count; j++)
+    {
+        Console.Write(l1[i][j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+Console.WriteLine();
+
+matrix.Show();
+
+matrix.matrix = matrix + matrix;
+
+matrix.Show();
+
+matrix++;
+
+matrix.Show();
+
+matrix--;
+
+matrix.Show();
+
+matrix.matrix = matrix * matrix;
+
+matrix.Show();
+
+matrix.matrix = matrix - matrix;
+
+matrix.Show();
